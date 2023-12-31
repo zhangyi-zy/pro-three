d7b7d07cff410c1bc42fe2ea4971f402ab4fea11
zhangyi <1055198744@qq.com>
2024-01-01 00:17:10 +0800

    pl

--- src/main/java/com/zy/test/Test2.java
+++ src/main/java/com/zy/test/Test2.java
@@ -5,10 +5,8 @@ public class Test2 {
         System.out.println("abcd");
         System.out.println("abcdef");
         System.out.println("abcdef");
-<<<<<<< HEAD
         System.out.println("abcdef99");
-=======
         System.out.println("abcdef878");
->>>>>>> 9aa7f536711652a3bf77f11bca157e7e2cedd883
+
     }
 }
