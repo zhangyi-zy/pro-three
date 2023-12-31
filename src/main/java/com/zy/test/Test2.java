fbdae271e4542496211a1fe1303fdb2329cf0984
zhangyi <1055198744@qq.com>
2024-01-01 00:08:05 +0800

    gg

--- src/main/java/com/zy/test/Test2.java
+++ src/main/java/com/zy/test/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("abcd");
         System.out.println("abcdef");
-        System.out.println("abcdef177");
-        System.out.println("abcdef99177");
+        System.out.println("abcdef1");
+        System.out.println("abcdef991");
     }
 }
