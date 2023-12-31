e42d8d12001b0d9b9ac92d605e5bbdfadd255b7e
zhangyi <1055198744@qq.com>
2024-01-01 00:15:03 +0800

    pl

--- src/main/java/com/zy/test/Test2.java
+++ src/main/java/com/zy/test/Test2.java
@@ -4,7 +4,11 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("abcd");
         System.out.println("abcdef");
-        System.out.println("abcdef177");
-        System.out.println("abcdef99177");
+        System.out.println("abcdef");
+<<<<<<< HEAD
+        System.out.println("abcdef99");
+=======
+        System.out.println("abcdef878");
+>>>>>>> 9aa7f536711652a3bf77f11bca157e7e2cedd883
     }
 }
--- src/main/java/com/zy/test/test4.java
+++
@@ -1,7 +0,0 @@
-package com.zy.test;
-
-public class test4 {
-    public static void main(String[] args) {
-        System.out.println("123");
-    }
-}
