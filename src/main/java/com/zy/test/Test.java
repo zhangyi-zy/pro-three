c083de73092123dec6c0ce78ae8f82c161644632
zhangyi <1055198744@qq.com>
2024-01-01 19:02:05 +0800

    Revert "uuu"
    This reverts commit 574839d9

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -2,6 +2,9 @@ package com.zy.test;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("hello!");
+        System.out.println("123456");
+        System.out.println("123456");
         System.out.println("11");
     }
 }
--- src/main/java/com/zy/test/Test3.java
+++ src/main/java/com/zy/test/Test3.java
@@ -3,5 +3,7 @@ package com.zy.test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("33");
+        System.out.println("abcdeee");
+        System.out.println("abcdeee");
     }
 }
