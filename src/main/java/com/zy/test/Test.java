3797bfc33044e8d5329e756737c315cd4f702025
zhangyi <1055198744@qq.com>
2024-01-01 17:51:00 +0800

    ttt

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("123456");
+        System.out.println("123456");
     }
 }
