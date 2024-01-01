099fe2c25e80aacb1a79e44136b5e763b16bc579
zhangyi <1055198744@qq.com>
2024-01-01 17:44:45 +0800

    yyy

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -3,8 +3,5 @@ package com.zy.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello!");
-        System.out.println("123456");
-        System.out.println("123456");
-        System.out.println("123456");
     }
 }
--- src/main/java/com/zy/test/Test3.java
+++ src/main/java/com/zy/test/Test3.java
@@ -3,6 +3,5 @@ package com.zy.test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("abcdeee");
-        System.out.println("ddddddd");
     }
 }
