a7dd8dbc755efddcab6716c9e2cd4b796533fa98
zhangyi <1055198744@qq.com>
2024-01-01 17:38:13 +0800

    rrr

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -4,7 +4,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("123456");
-        System.out.println("123456");
-        System.out.println("123456");
     }
 }
--- src/main/java/com/zy/test/Test3.java
+++ src/main/java/com/zy/test/Test3.java
@@ -4,7 +4,5 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("abcdeee");
         System.out.println("ddddddd");
-        System.out.println("ddddddd");
-        System.out.println("ddddddd");
     }
 }
