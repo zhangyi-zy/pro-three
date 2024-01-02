1e0d4152ac712ff0b8e92735d654107b60b0917f
zhangyi <1055198744@qq.com>
2024-01-03 00:18:24 +0800

    kk

--- src/main/java/com/zy/test/Test2.java
+++ src/main/java/com/zy/test/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("abcd");
 
+
     }
 }
--- src/main/java/com/zy/test/Test3.java
+++ src/main/java/com/zy/test/Test3.java
@@ -4,5 +4,6 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("abcdeee");
 
+
     }
 }
